ece4fd77f506593965e005f7a4a30f03fb4d3fe8
Orodan <jimmykasprzak@gmail.com>
2021-01-02 23:58:30 +0100

    ✨ add open css class when clicking on hamburger menu button

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log('javascript connected')
+const hamburgerButton = document.querySelector('#hamburger-button')
+
+hamburgerButton.addEventListener('click', () => {
+    if (hamburgerButton.classList.contains('open')) {
+        hamburgerButton.classList.remove('open')
+    } else {
+        hamburgerButton.classList.add('open')
+    }
+})
