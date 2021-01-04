d8cb90bebf5b2db85e3b1be26628590383937b04
Orodan <jimmykasprzak@gmail.com>
2021-01-04 23:31:56 +0100

    🐛  use css visibility property to create fade-out animation + refactor fade-in

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,17 @@
 const hamburgerButton = document.querySelector('#hamburger-button')
 const header = document.querySelector('header')
+const overlay = document.querySelector('.overlay')
 
 hamburgerButton.addEventListener('click', () => {
     if (header.classList.contains('menu-open')) {
         header.classList.remove('menu-open')
+
+        overlay.classList.remove('fade-in')
+        overlay.classList.add('fade-out')
     } else {
         header.classList.add('menu-open')
+
+        overlay.classList.add('fade-in')
+        overlay.classList.remove('fade-out')
     }
 })
