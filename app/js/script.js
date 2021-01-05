31e9f1710276a80fba4e879aa216d03f3ad5ddc7
Orodan <jimmykasprzak@gmail.com>
2021-01-05 23:44:58 +0100

    💄  implements mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 const hamburgerButton = document.querySelector('#hamburger-button')
 const header = document.querySelector('header')
 const overlay = document.querySelector('.overlay')
+const mobileMenu = document.querySelector('.header__menu')
 
 hamburgerButton.addEventListener('click', () => {
     if (header.classList.contains('menu-open')) {
@@ -8,10 +9,16 @@ hamburgerButton.addEventListener('click', () => {
 
         overlay.classList.remove('fade-in')
         overlay.classList.add('fade-out')
+
+        mobileMenu.classList.remove('fade-in')
+        mobileMenu.classList.add('fade-out')
     } else {
         header.classList.add('menu-open')
 
         overlay.classList.add('fade-in')
         overlay.classList.remove('fade-out')
+
+        mobileMenu.classList.add('fade-in')
+        mobileMenu.classList.remove('fade-out')
     }
 })
