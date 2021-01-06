f04f602c51d852226bd5bc1e9d57c5fd7d12b5c8
Orodan <jimmykasprzak@gmail.com>
2021-01-06 17:45:19 +0100

    ✨ block scroll when mobile menu is open

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body')
 const hamburgerButton = document.querySelector('#hamburger-button')
 const header = document.querySelector('header')
 const overlay = document.querySelector('.overlay')
@@ -6,6 +7,7 @@ const mobileMenu = document.querySelector('.header__menu')
 hamburgerButton.addEventListener('click', () => {
     if (header.classList.contains('menu-open')) {
         header.classList.remove('menu-open')
+        body.classList.remove('no-scroll')
 
         overlay.classList.remove('fade-in')
         overlay.classList.add('fade-out')
@@ -14,6 +16,7 @@ hamburgerButton.addEventListener('click', () => {
         mobileMenu.classList.add('fade-out')
     } else {
         header.classList.add('menu-open')
+        body.classList.add('no-scroll')
 
         overlay.classList.add('fade-in')
         overlay.classList.remove('fade-out')
